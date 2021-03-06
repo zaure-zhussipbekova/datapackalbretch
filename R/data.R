e97204d2b5073c54ed97c67f06fd3fc9f1645195
unknown <zaure.zhussipbekova@gmail.com>
2021-03-07 00:21:50 +0200

    second commit

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' Data of effort and size for training
+#'
+#' No missing values
+#' A dataset containing -----  whatever you put here.
+#'
+#' @title DATASET OF ...............
+#' @format A data frame with ....... rows and .......... variables:
+#' \describe{
+#' \item{effort}{Effort measured in -------.}
+#' \item{size}{Size measured in ....}
+#' \item{source}{whatever indicating one source or another.}
+#' }
+#' @source \url{https://....domain.com... }
+"trainfile"
+
+#' Data of effort and size for training
+#'
+#' No missing values
+#' A dataset containing -----  whatever you put here.
+#'
+#' @title DATASET OF ...............
+#' @format A data frame with ....... rows and .......... variables:
+#' \describe{
+#' \item{effort}{Effort measured in -------.}
+#' \item{size}{Size measured in ....}
+#' \item{source}{whatever indicating one source or another.}
+#' }
+#' @source \url{https://....domain.com... }
+"testfile"
